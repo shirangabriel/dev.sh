ccac5c183e879d3959c15c7a14ac87b6345c2f1f
Shiran Gabriel <gabriel.stcb@gmail.com>
2023-02-16 13:37:24 +0530

    Missing script added

--- dev.sh
+++ dev.sh
@@ -1,12 +1,21 @@
-### Android setting enviroment#####
-echo 'export ANDROID_SDK="$HOME/Library/Android/sdk"' >> ~/.zshrc
-echo 'export PATH="$HOME/Library/Android/sdk/platform-tools":$PATH' >> ~/.zshrc
+
+#Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
 ####### Xcodes App ########
 brew install --cask xcodes
 
 xcode-select --install # install xcode command line tools
+
+
+### Android & Java
+brew install --cask adoptopenjdk # Java
+brew install --cask android-studio # Android Studio
+
+# Android setting enviroment
+echo 'export ANDROID_SDK="$HOME/Library/Android/sdk"' >> ~/.zshrc
+echo 'export PATH="$HOME/Library/Android/sdk/platform-tools":$PATH' >> ~/.zshrc
 
 
 
@@ -18,10 +27,16 @@ echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
 
 source $(brew --prefix nvm)/nvm.sh
 
+nvm install 16
+nvm alias default 16 # set default
+
+
 #   nvm install node     ## Installing Latest version 
 #   nvm install 14       ## Installing Node.js 14.X version 
 #   nvm alias default 16.14.2  ## make default
 #   nvm use
+
+
 
 
 ####### Ruby ########
@@ -30,7 +45,8 @@ gpg --keyserver hkp://keys.gnupg.net  --recv-keys 409B6B1796C275462A1703113804BB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"'  >> ~/.zshrc
 
-#   rvm install 3.0
+rvm install 2 3.0 # this will install ruby v2 and v3
+rvm --default use 3.0n # set default
 
 
 ####### VS CODE ########
@@ -43,7 +59,7 @@ EOF
 # install 'code' command in shell path
 export PATH="$HOME/env:$PATH"
 
-
+# Plugins for VSCode
 code --install-extension formulahendry.auto-rename-tag   #Auto Rename Tag
 code --install-extension WallabyJs.quokka-vscode #Quokka.js
 code --install-extension esbenp.prettier-vscode  #Prettier
